118095b4c4420ae7d6b6524602b609536e4791db
Ryan Pavlik <rpavlik@gmail.com>
2013-05-07 16:54:10 -0500

    Some fixes .. asd reordering, definition loading

--- src/package.lisp
+++ src/package.lisp
@@ -1,16 +1,6 @@
-
 ;;;; package.lisp
 
 (defpackage #:sdl2
   (:use #:cl #:cffi))
 
-(in-package #:sdl2)
-
-(define-foreign-library libsdl2
-  (t (:default "libSDL2")))
-
-(use-foreign-library libsdl2)
-
-(load "sdl2-cffi.lisp")
-(load "sdl2-macros.lisp")
-
+(defpackage #:sdl2-ffi)
