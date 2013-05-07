a3e70ba47a1876a1f1d7551006d13d7a69daf38e
Chip <photex@gmail.com>
2013-05-07 16:55:54 -0700

    - init and quit in-place

--- src/package.lisp
+++ src/package.lisp
@@ -1,6 +1,15 @@
 ;;;; package.lisp
 
 (defpackage #:sdl2
-  (:use #:cl #:cffi))
+  (:use #:cl #:cffi)
+  (:export :sys-video
+           :sys-audio
+           :sys-timer
+           :sys-joystick
+           :sys-gamecontroller ; implies joystick
+           :noparachute
+           :everything
+           :init
+           :quit))
 
 (defpackage #:sdl2-ffi)
