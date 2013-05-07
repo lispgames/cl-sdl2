c2d9897dd3b6c4863a915e295df0d0d9d44ad708
Chip <photex@gmail.com>
2013-05-07 16:28:10 -0700

    - added some sdl constants

--- src/library.lisp
+++ src/library.lisp
@@ -5,4 +5,15 @@
 (define-foreign-library libsdl2
   (t (:default "libSDL2")))
 
-#+-(use-foreign-library libsdl2)
+(use-foreign-library libsdl2)
+
+(defconstant SDL-INIT-TIMER          #x00000001)
+(defconstant SDL-INIT-AUDIO          #x00000010)
+(defconstant SDL-INIT-VIDEO          #x00000020)
+(defconstant SDL-INIT-JOYSTICK       #x00000200)
+(defconstant SDL-INIT-HAPTIC         #x00001000)
+(defconstant SDL-INIT-GAMECONTROLLER #x00002000)
+(defconstant SDL-INIT-NOPARACHUTE    #x00100000)
+(defconstant SDL-INIT-EVERYTHING     #x0000FFFF)
+
+
