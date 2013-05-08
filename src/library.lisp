d2572d73031a74d455315bbfb95d1920c1c086f6
Chip <photex@gmail.com>
2013-05-07 17:08:55 -0700

    - yep, put that shit into the wrong file! yay!

--- src/library.lisp
+++ src/library.lisp
@@ -8,22 +8,3 @@
 
 (use-foreign-library libsdl2)
 
-(defconstant SYS-TIMER          #x00000001)
-(defconstant SYS-AUDIO          #x00000010)
-(defconstant SYS-VIDEO          #x00000020)
-(defconstant SYS-JOYSTICK       #x00000200)
-(defconstant SYS-HAPTIC         #x00001000)
-(defconstant SYS-GAMECONTROLLER #x00002000)
-(defconstant NOPARACHUTE        #x00100000)
-(defconstant EVERYTHING         #x0000FFFF)
-
-(defun init (&rest flags)
-  (let ((init-flags (if flags
-                        (apply #'logior flags)
-                        everything)))
-    (let ((rc (sdl2-ffi::sdl-init init-flags)))
-      (when (< rc 0)
-        (error (sdl2-ffi::sdl-geterror))))))
-
-(defun quit ()
-  (sdl2-ffi::sdl-quit))
--- src/sdl2.lisp
+++ src/sdl2.lisp
@@ -4,3 +4,22 @@
 
 ;;; "sdl2" goes here. Hacks and glory await!
 
+(defconstant SYS-TIMER          #x00000001)
+(defconstant SYS-AUDIO          #x00000010)
+(defconstant SYS-VIDEO          #x00000020)
+(defconstant SYS-JOYSTICK       #x00000200)
+(defconstant SYS-HAPTIC         #x00001000)
+(defconstant SYS-GAMECONTROLLER #x00002000)
+(defconstant NOPARACHUTE        #x00100000)
+(defconstant EVERYTHING         #x0000FFFF)
+
+(defun init (&rest flags)
+  (let ((init-flags (if flags
+                        (apply #'logior flags)
+                        everything)))
+    (let ((rc (sdl2-ffi::sdl-init init-flags)))
+      (when (< rc 0)
+        (error (sdl2-ffi::sdl-geterror))))))
+
+(defun quit ()
+  (sdl2-ffi::sdl-quit))
