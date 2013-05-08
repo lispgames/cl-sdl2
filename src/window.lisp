79f435a29b9ddef57c4995aafb33a5a28060ea6d
Ryan Pavlik <rpavlik@gmail.com>
2013-05-08 11:29:00 -0500

    Some conversion to bitflags and utilities for doing so

--- src/window.lisp
+++ src/window.lisp
@@ -1,29 +1,16 @@
 (in-package #:sdl2)
 
-(defconstant windowpos-centered 0)
+(defbitfield-from-cenum (sdl-window-flags
+                         sdl2-ffi:sdl-windowflags
+                         "SDL-WINDOW-")
+  (:centered #x0))
 
-(defconstant window-fullscreen #x00000001)
-(defconstant window-fullscreen-desktop (logior window-fullscreen #x00001000))
-(defconstant window-opengl #x00000002)
-(defconstant window-shown #x00000004)
-(defconstant window-hidden #x00000008)
-(defconstant window-borderless #x00000010)
-(defconstant window-resizable #x00000020)
-(defconstant window-minimized #x00000040)
-(defconstant window-maximized #x00000080)
-(defconstant window-input-grabbed #x00000100)
-(defconstant window-input-focus #x00000200)
-(defconstant window-mouse-focus #x00000400)
-
-
-(defun create-window (&optional
+(defun create-window (&key
                         (title "SDL2 Window")
                         (x windowpos-centered) (y windowpos-centered)
-                        (w 800) (h 600)
-                      &rest flags)
-  (let ((window-flags (if flags (apply #'logior flags)
-                          (list window-shown))))
-    (sdl2-ffi::sdl-createwindow title x y w h window-flags)))
+                        (w 800) (h 600) flags)
+  (let ((window-flags (foreign-bitfield-value 'sdl-window-flags flags)))
+    (sdl2-ffi:sdl-createwindow title x y w h window-flags)))
 
 (defun destroy-window (win)
-  (sdl2-ffi::sdl-destroywindow win))
+  (sdl2-ffi:sdl-destroywindow win))
