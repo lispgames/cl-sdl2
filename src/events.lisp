2c60b33593365ed6501e16d2ebfb193ed304daef
Chip <photex@gmail.com>
2013-05-15 11:26:24 -0700

    - reorg - renamed with-foreign-event to with-sdl-event - updated next-event to correctly avoid calling get-event-data when there is no event - tinkered wildly with get-event-data - get-event-struct-data now also returns the event-type

--- src/events.lisp
+++ src/events.lisp
@@ -86,23 +86,44 @@
 (defun free-event (event-ptr)
   (foreign-free event-ptr))
 
+(defmacro with-sdl-event ((event-ptr &optional (event-type :firstevent))
+                          &body body)
+  `(let* ((,event-ptr (new-event))
+          (result (progn ,@body)))
+     (free-event ,event-ptr)
+     result))
+
 (defun get-event-type (event-ptr)
   (let ((enum-value (foreign-slot-value event-ptr 'sdl2-ffi:sdl-event 'type)))
     (foreign-enum-keyword 'event-type enum-value)))
 
 (defun get-event-struct-data (event-ptr)
   (let* ((event-type (get-event-type event-ptr)))
-    (gethash event-type *event-struct-data*)))
+    (list event-type (gethash event-type *event-struct-data*))))
 
 (defun get-event-data (event-ptr)
-  (let* ((event-struct-type (second (get-event-struct-data event-ptr))))
-    (when event-struct-type
-      (let ((slot-names (foreign-slot-names event-struct-type)))
-        (mapcan #'list
-                (mapcar #'alexandria:make-keyword slot-names)
-                (loop for slot-name in slot-names
-                   collect
-                     (foreign-slot-value event-ptr event-struct-type slot-name)))))))
+  (destructuring-bind (event-type (event-struct-slot event-struct-type))
+      (get-event-struct-data event-ptr)
+    (let* ((event-struct-ptr (foreign-slot-value event-ptr
+                                                 'sdl2-ffi:sdl-event
+                                                 event-struct-slot)))
+      (when event-struct-type
+        (let* ((slot-names (foreign-slot-names event-struct-type))
+               (result (mapcan #'list
+                               (mapcar #'alexandria:make-keyword slot-names)
+                               (loop for slot-name in slot-names
+                                  collect (foreign-slot-value event-struct-ptr
+                                                              event-struct-type
+                                                              slot-name)))))
+          (setf (getf result :type) event-type)
+          result)))))
+
+(defun pump-events ()
+  (sdl2-ffi:sdl-pumpevents))
+
+(defun push-event (event-type)
+  (check-rc (with-sdl-event (event-ptr event-type)
+              (sdl2-ffi:sdl-pushevent event-ptr))))
 
 (defun next-event (event-ptr &optional (method :poll) (timeout 1))
   "Method can be either :poll, :wait, or :wait-with-timeout"
@@ -113,27 +134,22 @@
                       (lambda (e)
                         (sdl2-ffi:sdl-waiteventtimeout e timeout))))))
     (let* ((eventp (funcall event-fn event-ptr))
-           (event (list eventp (when eventp (get-event-data event-ptr)))))
+           (event (list eventp (unless (= 0 eventp) (get-event-data event-ptr)))))
       event)))
 
-(defmacro with-foreign-event ((event-ptr) &body body)
-  `(let* ((,event-ptr (new-event))
-          (result (progn ,@body)))
-     (free-event ,event-ptr)
-     result))
+;;; The following three functions shouldn't be used in a real
+;;; game loop. They are simply here for easier testing and
+;;; experimentation.
 
 (defun poll-event ()
-  (with-foreign-event (event-ptr)
+  (with-sdl-event (event-ptr)
     (next-event event-ptr)))
 
 (defun wait-event ()
-  (with-foreign-event (event-ptr)
+  (with-sdl-event (event-ptr)
     (next-event event-ptr :wait)))
 
 (defun wait-event-timeout (timeout)
-  (with-foreign-event (event-ptr)
+  (with-sdl-event (event-ptr)
     (next-event event-ptr :wait-with-timeout timeout)))
-
-(defun pump-events ()
-  (sdl2-ffi:sdl-pumpevents))
 
