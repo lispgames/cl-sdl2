752d89930f77ff76d566d20fde876382721fe3a7
Chip <photex@gmail.com>
2013-06-12 15:39:35 -0700

    - basic test is now at least a bit more interesting visually

--- examples/basic.lisp
+++ examples/basic.lisp
@@ -1,21 +1,37 @@
 (in-package :sdl2-examples)
 
 (require :sdl2)
+(require :cl-opengl)
 
 (defun basic-test ()
   (sdl2:with-init (:video)
     (let* ((win (sdl2:create-window :flags '(:shown :opengl)))
            (gl-context (sdl2:gl-create-context win)))
       (sdl2:gl-make-current win gl-context)
+      (gl:viewport 0 0 800 600)
+      (gl:matrix-mode :projection)
+      (gl:ortho -2 2 -2 2 -2 2)
+      (gl:matrix-mode :modelview)
+      (gl:load-identity)
+      (gl:clear-color 0.0 0.0 1.0 1.0)
+      (gl:clear :color-buffer)
       (sdl2:with-event-loop (:method :poll)
         (:keyup ()
                 (sdl2:push-event :quit))
         (:mousemotion (:x x :y y :xrel xrel :yrel yrel :state state)
                       (print (format
                               nil
-                              "Mouse motion abs(rel): ~a (~a), ~a (~a)~%Mouse state: ~a~%"
+                              "Mouse motion abs(rel): ~a (~a), ~a (~a)~%Mouse state: ~a"
                               x xrel y yrel state)))
         (:idle ()
+               (gl:clear :color-buffer)
+               (gl:begin :triangles)
+               (gl:color 1.0 0.0 0.0)
+               (gl:vertex 0.0 1.0)
+               (gl:vertex -1.0 -1.0)
+               (gl:vertex 1.0 -1.0)
+               (gl:end)
+               (gl:flush)
                (sdl2:gl-swap-window win))
         (:quit ()
                t))
