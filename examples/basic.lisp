7c3ab134464a0f7165f8cb288ed587546231e4f6
Chip Collier <photex@lofidelitygames.com>
2013-06-12 22:19:08 -0700

    - added keyboard.lisp - updated basic-test

--- examples/basic.lisp
+++ examples/basic.lisp
@@ -16,9 +16,24 @@
       (gl:clear-color 0.0 0.0 1.0 1.0)
       (gl:clear :color-buffer)
       (sdl2:with-event-loop (:method :poll)
+        (:keydown (:keysym keysym)
+                  (let ((scancode (sdl2:scancode-value keysym))
+                        (sym (sdl2:sym-value keysym))
+                        (mod-value (sdl2::mod-value keysym)))
+                  (if (= sym sdl2::+SDLK-W+)
+                      (if (and
+                           (sdl2::mod-value-p mod-value :kmod-rctrl :kmod-lctrl)
+                           (sdl2::mod-value-p mod-value :kmod-rshift :kmod-lshift))
+                          (print "BACKWARD")
+                          (print "FORWARD"))
+                      (progn
+                        (print (format nil "Key sym: ~a, code: ~a, mod: ~a"
+                                       sym
+                                       scancode
+                                       mod-value))))))
         (:keyup (:keysym keysym)
-                (print (format nil "Key sym value: ~a" keysym))
-                (sdl2:push-event :quit))
+                (when (equal :sdl-scancode-escape (sdl2:scancode-value keysym))
+                  (sdl2:push-event :quit)))
         (:mousemotion (:x x :y y :xrel xrel :yrel yrel :state state)
                       (print (format
                               nil
---
+++ src/keyboard.lisp
@@ -0,0 +1,31 @@
+(in-package #:sdl2)
+
+(defbitfield-from-cenum (keymod sdl2-ffi::sdl-keymod "SDL-"))
+
+(defmacro keysym-slot-value (sdl-keysym-ptr slot-name)
+  `(foreign-slot-value ,sdl-keysym-ptr
+                       ;; TODO why do I need this? I get an error
+                       ;; if I use 'sdl2-ffi::sdl-keysym even though
+                       ;; that's what this function returns
+                       (foreign-slot-type 'sdl2-ffi::sdl-keyboardevent
+                                          'sdl2-ffi::keysym)
+                       ,slot-name))
+
+(defun scancode-value (sdl-keysym-ptr)
+  (keysym-slot-value sdl-keysym-ptr 'sdl2-ffi::scancode))
+
+(defun sym-value (sdl-keysym-ptr)
+  (keysym-slot-value sdl-keysym-ptr 'sdl2-ffi::sym))
+
+(defun mod-value (sdl-keysym-ptr)
+  (keysym-slot-value sdl-keysym-ptr 'sdl2-ffi::mod))
+
+(defun mod-keywords (value)
+  ;; using rest here because :kmod-none is always first
+  ;; TODO is it possible that I'm using it incorrectly?
+  (rest (foreign-bitfield-symbols 'keymod value)))
+
+(defun mod-value-p (value &rest keywords)
+  (let ((bitfield-value (foreign-bitfield-value 'keymod keywords)))
+    (not (= 0 (logand bitfield-value value)))))
+
