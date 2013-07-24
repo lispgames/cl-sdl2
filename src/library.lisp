023e5177855f9b6c119732946ae775d9e3863f4e
Ryan Pavlik <rpavlik@gmail.com>
2013-07-24 15:07:37 -0500

    Migration to autowrap

--- src/library.lisp
+++ src/library.lisp
@@ -1,11 +1,9 @@
 (in-package :sdl2)
 
-(defctype sdl2-ffi::size-t :unsigned-int)
-
-(define-foreign-library libsdl2
+(cffi:define-foreign-library libsdl2
   (:darwin (:default "libSDL2"))
   (:unix (:or "libSDL2-2.0.so.0" "libSDL2"))
   (t (:default "libSDL2")))
 
-(use-foreign-library libsdl2)
+(cffi:use-foreign-library libsdl2)
 
--- src/sdl2.lisp
+++ src/sdl2.lisp
@@ -13,80 +13,47 @@
              (with-slots (code string) c
               (format s "SDL Error (~A): ~A" code string)))))
 
-(define-condition sdl-invalid-object-error (sdl-error)
-  ((object :initarg :object :initform nil :accessor sdl-invalid-object))
-  (:report (lambda (c s)
-             (with-slots (object) c
-               (format s "SDL Error: Invalid/Destroyed Object: ~A" object)))))
-
-(defstruct sdl-wrapped-ptr
-  (ptr (null-pointer) :type #.(type-of (null-pointer)))
-  (valid-p t :type boolean))
-
-(declaim (inline sdl-ptr sdl-invalidate))
-(defun sdl-ptr (wrapped-ptr)
-  (if (sdl-wrapped-ptr-valid-p wrapped-ptr)
-      (sdl-wrapped-ptr-ptr wrapped-ptr)
-      (error 'sdl-invalid-object-error :object wrapped-ptr)))
-
-(defun sdl-invalidate (wrapped-ptr)
-  (setf (sdl-wrapped-ptr-valid-p wrapped-ptr) nil)
-  (sdl-wrapped-ptr-ptr wrapped-ptr))
-
-(defmethod print-object ((object sdl-wrapped-ptr) stream)
-  (print-unreadable-object (object stream :type t :identity nil)
-    (format stream "{#X~8,'0X}" (pointer-address (sdl-wrapped-ptr-ptr object)))))
-
 (defun sdl-collect (wrapped-ptr &optional (free-fun #'foreign-free))
-  (let ((ptr (sdl-ptr wrapped-ptr)))
-    (trivial-garbage:finalize wrapped-ptr
-                              (lambda () (funcall free-fun ptr)))
+  (let ((ptr (autowrap:ptr wrapped-ptr)))
+    (tg:finalize wrapped-ptr (lambda () (funcall free-fun ptr)))
     wrapped-ptr))
 
 (defun sdl-cancel-collect (wrapped-ptr)
-  (trivial-garbage:cancel-finalization wrapped-ptr)
+  (tg:cancel-finalization wrapped-ptr)
   wrapped-ptr)
 
-(defmacro defwrapper (wrapped-struct (&optional ffi-type)
-                      &body more-slots)
-  `(progn
-     (defstruct (,wrapped-struct (:include sdl-wrapped-ptr)
-                                 (:constructor ,(symbolicate "%MAKE-" wrapped-struct)))
-       ,@more-slots)
-     ,@(when ffi-type
-         `((make-struct-accessors ,ffi-type ,wrapped-struct)))))
-
-(defbitfield* sdl-init-flags
-  (:timer          sdl2-ffi:+sdl-init-timer+)
-  (:audio          sdl2-ffi:+sdl-init-audio+)
-  (:video          sdl2-ffi:+sdl-init-video+)
-  (:joystick       sdl2-ffi:+sdl-init-joystick+)
-  (:haptic         sdl2-ffi:+sdl-init-haptic+)
-  (:gamecontroller sdl2-ffi:+sdl-init-gamecontroller+)
-  (:noparachute    sdl2-ffi:+sdl-init-noparachute+)
-  (:everything     #x0000FFFF))
+(autowrap:define-bitmask-from-constants (sdl-init-flags)
+  sdl2-ffi:+sdl-init-timer+
+  sdl2-ffi:+sdl-init-audio+
+  sdl2-ffi:+sdl-init-video+
+  sdl2-ffi:+sdl-init-joystick+
+  sdl2-ffi:+sdl-init-haptic+
+  sdl2-ffi:+sdl-init-gamecontroller+
+  sdl2-ffi:+sdl-init-noparachute+
+  '(:everything . #x0000FFFF))
 
 (defmacro check-rc (form)
   (with-gensyms (rc)
     `(let ((,rc ,form))
        (when (< ,rc 0)
-         (error 'sdl-rc-error :rc ,rc :string (sdl2-ffi:sdl-geterror))))))
+         (error 'sdl-rc-error :rc ,rc :string (sdl-get-error)))
+       ,rc)))
 
 (defmacro check-null (form)
-  (with-gensyms (ptr)
-    `(let ((,ptr ,form))
-       (if (null-pointer-p ,ptr)
-           (error 'sdl-rc-error :rc ,ptr :string (sdl2-ffi:sdl-geterror))
-           ,ptr))))
+  (with-gensyms (wrapper)
+    `(let ((,wrapper ,form))
+       (if (null-pointer-p (autowrap:ptr ,wrapper))
+           (error 'sdl-rc-error :rc ,wrapper :string (sdl-get-error))
+           ,wrapper))))
 
 (defun init (&rest sdl-init-flags)
   "Initialize SDL2 with the specified subsystems. Initializes everything by default."
-  (let ((init-flags (foreign-bitfield-value 'sdl-init-flags sdl-init-flags)))
-    (check-rc (sdl2-ffi:sdl-init init-flags))))
+  (let ((init-flags (autowrap:mask-apply 'sdl-init-flags sdl-init-flags)))
+    (check-rc (sdl-init init-flags))))
 
 (defun quit ()
   "Shuts down SDL2."
-  (sdl2-ffi::sdl-quit))
+  (sdl-quit))
 
 (defmacro with-init ((&rest sdl-init-flags) &body body)
   `(progn
