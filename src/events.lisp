ec182dc666b7fb9b5ac77789a6a7a50c0d227180
Chip Collier <photex@lofidelitygames.com>
2013-08-28 22:12:50 -0700

    - I didn't change anything, but I think emacs just did a big auto-indent

--- src/events.lisp
+++ src/events.lisp
@@ -94,10 +94,10 @@
                     (binding (second param)))
                 `(,binding (,(let* ((*package* (find-package :sdl2))
                                     (accessor (symbolicate accessor-prefix keyword)))
-                               (if (fboundp accessor)
-                                   accessor
-                                   (error "Invalid event (~S) or slot (~S)" event-type keyword)))
-                            ,event-var))))
+                                   (if (fboundp accessor)
+                                       accessor
+                                       (error "Invalid event (~S) or slot (~S)" event-type keyword)))
+                             ,event-var))))
             params)))
 
 (defun expand-handler (sdl-event event-type params forms)
@@ -121,22 +121,22 @@
        (with-sdl-event (,sdl-event)
          (setf ,idle-func #'(lambda () ,@(expand-idle-handler event-handlers)))
          (loop until ,quit
-               do (loop until (= 0 (next-event ,sdl-event ,method ,timeout))
-                        do (case (get-event-type ,sdl-event)
-                             ,@(remove nil
-                                (mapcar
-                                 #'(lambda (handler)
-                                     (if (eq (first handler) :quit)
-                                         (expand-quit-handler sdl-event
-                                                              (rest (rest handler))
-                                                              quit)
-                                         (let* ((event-type (first handler))
-                                                (params (second handler))
-                                                (forms (rest (rest handler))))
-                                           (expand-handler sdl-event
-                                                           event-type
-                                                           params
-                                                           forms))))
-                                 event-handlers))))
-                  (unless ,quit
-                    (funcall ,idle-func)))))))
+            do (loop until (= 0 (next-event ,sdl-event ,method ,timeout))
+                  do (case (get-event-type ,sdl-event)
+                       ,@(remove nil
+                                 (mapcar
+                                  #'(lambda (handler)
+                                      (if (eq (first handler) :quit)
+                                          (expand-quit-handler sdl-event
+                                                               (rest (rest handler))
+                                                               quit)
+                                          (let* ((event-type (first handler))
+                                                 (params (second handler))
+                                                 (forms (rest (rest handler))))
+                                            (expand-handler sdl-event
+                                                            event-type
+                                                            params
+                                                            forms))))
+                                  event-handlers))))
+              (unless ,quit
+                (funcall ,idle-func)))))))
