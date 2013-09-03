14b2eab62fb9de90e6bd3b9285f4d657881d0d34
Chip Collier <photex@lofidelitygames.com>
2013-09-02 21:39:22 -0700

    - some reformatting because I'm tired of fighting emacs - updated basic-test to rumble when a gamecontroller button is pressed on a supported gamepad

--- examples/basic.lisp
+++ examples/basic.lisp
@@ -7,7 +7,8 @@
   (sdl2:with-init (:everything)
     (let* ((win (sdl2:create-window :flags '(:shown :opengl)))
            (gl-context (sdl2:gl-create-context win))
-           (controllers ()))
+           (controllers ())
+           (haptic ()))
 
       ;; basic window/gl setup
       (sdl2:gl-make-current win gl-context)
@@ -24,50 +25,75 @@
          do (when (sdl2:game-controller-p i)
               (format t "Found gamecontroller: ~a~%"
                       (sdl2:game-controller-name-for-index i))
-              (setf controllers (acons i (sdl2:game-controller-open i) controllers))))
+              (let* ((gc (sdl2:game-controller-open i))
+                     (joy (sdl2:game-controller-get-joystick gc)))
+                (setf controllers (acons i gc controllers))
+                (when (sdl2:joystick-is-haptic joy)
+                  (let ((h (sdl2:haptic-open-from-joystick joy)))
+                    (setf haptic (acons i h haptic))
+                    (sdl2:rumble-init h))))))
       
       (sdl2:with-event-loop (:method :poll)
-        (:keydown (:keysym keysym)
-          (let ((scancode (sdl2:scancode-value keysym))
-                (sym (sdl2:sym-value keysym))
-                (mod-value (sdl2:mod-value keysym)))
-            (if (sdl2:scancode= scancode :scancode-w)
-                (if (and
-                     (sdl2::mod-value-p mod-value :kmod-rctrl :kmod-lctrl)
-                     (sdl2::mod-value-p mod-value :kmod-rshift :kmod-lshift))
-                    (print "BACKWARD")
-                    (print "FORWARD"))
-                (progn
-                  (print (format nil "Key sym: ~a, code: ~a, mod: ~a"
-                                 sym
-                                 scancode
-                                 mod-value))))))
-        (:keyup (:keysym keysym)
-          (when (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-escape)
-            (sdl2:push-event :quit)))
-        (:mousemotion (:x x :y y :xrel xrel :yrel yrel :state state)
-          (print (format
-                  nil
-                  "Mouse motion abs(rel): ~a (~a), ~a (~a)~%Mouse state: ~a"
-                  x xrel y yrel state)))
-        (:controlleraxismotion (:which controller-id :axis axis-id :value value)
-           (format t "Controller axis motion: Controller: ~a, Axis: ~a, Value: ~a~%"
-                   controller-id axis-id value))
-        (:idle ()
-          (gl:clear :color-buffer)
-          (gl:begin :triangles)
-          (gl:color 1.0 0.0 0.0)
-          (gl:vertex 0.0 1.0)
-          (gl:vertex -1.0 -1.0)
-          (gl:vertex 1.0 -1.0)
-          (gl:end)
-          (gl:flush)
-          (sdl2:gl-swap-window win))
+        (:keydown
+         (:keysym keysym)
+         (let ((scancode (sdl2:scancode-value keysym))
+               (sym (sdl2:sym-value keysym))
+               (mod-value (sdl2:mod-value keysym)))
+           (if (sdl2:scancode= scancode :scancode-w)
+               (if (and
+                    (sdl2::mod-value-p mod-value :kmod-rctrl :kmod-lctrl)
+                    (sdl2::mod-value-p mod-value :kmod-rshift :kmod-lshift))
+                   (print "BACKWARD")
+                   (print "FORWARD"))
+               (progn
+                 (print (format nil "Key sym: ~a, code: ~a, mod: ~a"
+                                sym
+                                scancode
+                                mod-value))))))
+        
+        (:keyup
+         (:keysym keysym)
+         (when (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-escape)
+           (sdl2:push-event :quit)))
+        
+        (:mousemotion
+         (:x x :y y :xrel xrel :yrel yrel :state state)
+         (print (format
+                 nil
+                 "Mouse motion abs(rel): ~a (~a), ~a (~a)~%Mouse state: ~a"
+                 x xrel y yrel state)))
+        
+        (:controlleraxismotion
+         (:which controller-id :axis axis-id :value value)
+         (format t "Controller axis motion: Controller: ~a, Axis: ~a, Value: ~a~%"
+                 controller-id axis-id value))
+        
+        (:controllerbuttondown
+         (:which controller-id)
+         (let ((h (cdr (assoc controller-id haptic))))
+           (when h
+             (sdl2:rumble-play h 1.0 100))))
+        
+        (:idle
+         ()
+         (gl:clear :color-buffer)
+         (gl:begin :triangles)
+         (gl:color 1.0 0.0 0.0)
+         (gl:vertex 0.0 1.0)
+         (gl:vertex -1.0 -1.0)
+         (gl:vertex 1.0 -1.0)
+         (gl:end)
+         (gl:flush)
+         (sdl2:gl-swap-window win))
+        
         (:quit () t))
 
       ;; close any game controllers that were opened
-      (loop for (_ . controller) in controllers
-         do (sdl2:game-controller-close controller))
+      ;; as well as any haptics
+      (loop for (i . controller) in controllers
+         do (progn
+              (sdl2:game-controller-close controller)
+              (sdl2:haptic-close (cdr (assoc i haptic)))))
       
       (sdl2:gl-delete-context gl-context)
       (sdl2:destroy-window win))))
