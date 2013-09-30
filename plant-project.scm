3db9403cea4330f8b09e140d2e1620875b0df95e
Chip <photex@gmail.com>
2013-09-30 12:21:50 -0700

    - added a plant project description

---
+++ plant-project.scm
@@ -0,0 +1,15 @@
+((#:quickloads
+  "swank"
+  "alexandria"
+  "osicat"
+  "cl-fad"
+  "cl-ppcre"
+  "trivial-garbage"
+  "cl-opengl"
+  "optima"
+  "cl-autowrap")
+ (#:project-name . "cl-sdl2")
+ (#:project-version . "0.0.1")
+ (#:dependencies
+  ((#:method . "git")
+   (#:url . "git://github.com/rpav/cl-autowrap.git"))))
