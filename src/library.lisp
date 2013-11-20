c2534c136939c7e7a7fdd36b89a5985d1bbfd38d
Ryan Pavlik <rpavlik@gmail.com>
2013-11-19 18:11:26 -0600

    :framework loading for osx

--- src/library.lisp
+++ src/library.lisp
@@ -2,7 +2,7 @@
 
 #-:gamekit
 (cffi:define-foreign-library libsdl2
-    (:darwin (:default "libSDL2"))
+  (:darwin (:or (:framework "SDL2") (:default "libSDL2")))
   (:unix (:or "libSDL2-2.0.so.0" "libSDL2"))
   (:windows "SDL2.dll")
   (t (:default "libSDL2")))
