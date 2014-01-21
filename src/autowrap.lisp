611d121ea8a2259a76f83540d96a1bf197f23a2a
Ryan Pavlik <rpavlik@gmail.com>
2014-01-21 06:27:54 -0600

    Mute autowrap reporting

--- src/autowrap.lisp
+++ src/autowrap.lisp
@@ -2,20 +2,21 @@
 
 (autowrap:c-include
  '(sdl2 autowrap-spec "SDL2.h")
- :accessor-package :sdl2-ffi.accessors
- :function-package :sdl2-ffi.functions
- :spec-path '(sdl2 autowrap-spec)
- :exclude-sources ("/usr/local/lib/clang/3.3/include/(?!stddef.h)"
-                   "/usr/include/(?!stdint.h|bits/types.h|sys/types.h).*")
- :exclude-definitions ("SDL_LogMessageV"
-                       "SDL_vsnprintf"
-                       "_inline$"
-                       "^_mm_")
- :symbol-exceptions (("SDL_Log" . "SDL-LOGGER")
-                     ("SDL_log" . "SDL-LOGN")
-                     ("SDL_RWops" . "SDL-RWOPS")
-                     ("SDL_GLContext" . "SDL-GLCONTEXT")
-                     ("SDL_GLattr" . "SDL-GLATTR")
-                     ("SDL_GLprofile" . "SDL-GLPROFILE")
-                     ("SDL_GLcontextFlag" . "SDL-GLCONTEXT-FLAG")))
+  :accessor-package :sdl2-ffi.accessors
+  :function-package :sdl2-ffi.functions
+  :spec-path '(sdl2 autowrap-spec)
+  :exclude-sources ("/usr/local/lib/clang/3.3/include/(?!stddef.h)"
+                    "/usr/include/(?!stdint.h|bits/types.h|sys/types.h).*")
+  :exclude-definitions ("SDL_LogMessageV"
+                        "SDL_vsnprintf"
+                        "_inline$"
+                        "^_mm_")
+  :symbol-exceptions (("SDL_Log" . "SDL-LOGGER")
+                      ("SDL_log" . "SDL-LOGN")
+                      ("SDL_RWops" . "SDL-RWOPS")
+                      ("SDL_GLContext" . "SDL-GLCONTEXT")
+                      ("SDL_GLattr" . "SDL-GLATTR")
+                      ("SDL_GLprofile" . "SDL-GLPROFILE")
+                      ("SDL_GLcontextFlag" . "SDL-GLCONTEXT-FLAG"))
+  :release-p cl:t)
 
