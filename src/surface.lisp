8b36099fbb6a88ca10119637a6aa157f84fdf39e
Christopher Carpenter <mordocai@mordocai.net>
2015-11-05 23:43:38 -0600

    Revert back to superior format-format function

--- src/surface.lisp
+++ src/surface.lisp
@@ -15,8 +15,7 @@
 
 (defun surface-format-format (surface)
   (enum-key '(:enum (sdl-pixel-format))
-            (c-ref (c-ref surface sdl2-ffi::sdl-surface :format)
-                   sdl2-ffi::sdl-pixel-format :format)))
+            (c-ref surface sdl2-ffi:sdl-surface :format :format)))
 
 (defun create-rgb-surface (width height depth
                            &key (r-mask 0) (g-mask 0) (b-mask 0) (a-mask 0)
