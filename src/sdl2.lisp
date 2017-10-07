f2dfc97fcc5208d2259e7755faa3244d1be62841
Will <will.whitty.arbeit@gmail.com>
2017-10-07 18:49:19 +0300

    Refactored initialisation so that it's non-blocking & portable

--- src/sdl2.lisp
+++ src/sdl2.lisp
@@ -4,6 +4,22 @@
 
 ;;; "sdl2" goes here. Hacks and glory await!
 
+(defvar *main-thread* nil)
+(defvar *wakeup-event* nil)
+(defvar *the-main-thread* nil)
+(defvar *lisp-message-event* nil)
+(defvar *main-thread-channel* nil)
+
+(autowrap:define-bitmask-from-constants (sdl-init-flags)
+  sdl2-ffi:+sdl-init-timer+
+  sdl2-ffi:+sdl-init-audio+
+  sdl2-ffi:+sdl-init-video+
+  sdl2-ffi:+sdl-init-joystick+
+  sdl2-ffi:+sdl-init-haptic+
+  sdl2-ffi:+sdl-init-gamecontroller+
+  sdl2-ffi:+sdl-init-noparachute+
+  '(:everything . #x0000FFFF))
+
 (define-condition sdl-error (error)
   ((string :initarg :string :initform nil :accessor sdl-error-string))
   (:report (lambda (c s)
@@ -32,16 +48,6 @@
   "Use this function to convert truth from a low level wrapped SDL function
 returning an SDL_true into CL's boolean type system."
   (= (autowrap:enum-value 'sdl2-ffi:sdl-bool :true) integer-bool))
-
-(autowrap:define-bitmask-from-constants (sdl-init-flags)
-  sdl2-ffi:+sdl-init-timer+
-  sdl2-ffi:+sdl-init-audio+
-  sdl2-ffi:+sdl-init-video+
-  sdl2-ffi:+sdl-init-joystick+
-  sdl2-ffi:+sdl-init-haptic+
-  sdl2-ffi:+sdl-init-gamecontroller+
-  sdl2-ffi:+sdl-init-noparachute+
-  '(:everything . #x0000FFFF))
 
 ;;;
 ;;; NAMING CONVENTION: check-<foo>
@@ -90,11 +96,120 @@ returning an SDL_true into CL's boolean type system."
            (error 'sdl-error :string (sdl-get-error))
            ,v))))
 
-(defvar *the-main-thread* nil)
-(defvar *main-thread-channel* nil)
-(defvar *main-thread* nil)
-(defvar *lisp-message-event* nil)
-(defvar *wakeup-event* nil)
+(defun was-init (&rest flags)
+  (/= 0 (sdl-was-init (autowrap:mask-apply 'sdl-init-flags flags))))
+
+(defun niy (message)
+  (error "SDL2 Error: Construct Not Implemented Yet: ~A" message))
+
+(defun version ()
+  (c-let ((ver sdl2-ffi:sdl-version :free t))
+    (sdl-get-version (ver &))
+    (values (ver :major) (ver :minor) (ver :patch))))
+
+(defun version-wrapped ()
+  (values sdl2-ffi:+sdl-major-version+
+          sdl2-ffi:+sdl-minor-version+
+          sdl2-ffi:+sdl-patchlevel+))
+
+;;
+;; Message handling
+
+(defun recv-and-handle-message ()
+  (let ((msg (recvmsg *main-thread-channel*)))
+    (handle-message msg)))
+
+(defun get-and-handle-messages ()
+  (loop as msg = (and *main-thread-channel*
+                      (getmsg *main-thread-channel*))
+        while msg do
+          (handle-message msg)))
+
+(defmacro without-fp-traps (&body body)
+  #+sbcl
+  `(sb-int:with-float-traps-masked (:underflow
+                                    :overflow
+                                    :inexact
+                                    :invalid
+                                    :divide-by-zero) ,@body)
+  #-sbcl
+  `(progn ,@body))
+
+(defun handle-message (msg)
+  (let ((fun (car msg))
+        (chan (cdr msg))
+        (condition))
+    (handler-bind ((sdl-continue
+                     (lambda (c)
+                       (declare (ignore c))
+                       (when chan (sendmsg chan nil))
+                       (return-from handle-message)))
+                   (sdl-quit
+                     (lambda (c)
+                       (declare (ignore c))
+                       (quit)
+                       (return-from handle-message))))
+      (handler-bind ((error (lambda (e) (setf condition e))))
+        (if chan
+            (sendmsg chan (multiple-value-list (funcall fun)))
+            (funcall fun))))))
+
+;;
+;; Main thread helpers
+
+(defun find-main-thread ()
+  #+ccl ccl::*initial-process*
+  #+sbcl (sb-thread:main-thread)
+  #-(or sbcl ccl) (bt:current-thread))
+
+(defun ensure-main-channel ()
+  (unless *main-thread-channel*
+    (setf *main-thread-channel* (make-channel))))
+
+(defun sdl-main-thread ()
+  (without-fp-traps
+    (let ((*main-thread* (bt:current-thread))
+          #+sdl2::sdl2-swank (swank:*sldb-quit-restart* 'continue)
+          #+sdl2::sdl2-slynk (slynk:*sly-db-quit-restart* 'continue))
+      (loop while *main-thread-channel* do
+        (block loop-block
+          (restart-bind ((continue (lambda (&optional v)
+                                     (declare (ignore v))
+                                     (signal 'sdl-continue))
+                                   :report-function
+                                   (lambda (stream)
+                                     (format stream "Return to the SDL2 main loop.")))
+                         (abort (lambda (&optional v)
+                                  (declare (ignore v))
+                                  (signal 'sdl-quit))
+                                :report-function
+                                (lambda (stream)
+                                  (format stream "Abort, quitting SDL2 entirely."))))
+            (recv-and-handle-message)))))))
+
+(defun setup-main-thread ()
+
+  ;; Prevent re-calls
+  (when *the-main-thread*
+    (return-from setup-main-thread))
+
+  (unless *wakeup-event*
+    (setf *wakeup-event* (alloc 'sdl2-ffi:sdl-event)))
+  (unless *main-thread-channel*
+    (ensure-main-channel)
+
+    ;; If we did not have a main-thread, make one.
+    ;; Events and funcalls will be piped into this thread
+    #-darwin
+    (setf *the-main-thread* (bt:make-thread #'sdl-main-thread :name "SDL2 Main Thread"))
+
+    ;; On OSX, we need to run in the main thread
+    #+darwin
+    (setf *the-main-thread* (find-main-thread))))
+
+    ;; We'll interrupt this thread now in CCL and replace it with the sdl-main-loop
+    ;#+nil;(and darwin ccl)
+    ;(bt:interrupt-thread *the-main-thread* #'sdl-main-thread)))
 
 (defmacro in-main-thread ((&key background no-event) &body b)
   (with-gensyms (fun channel)
@@ -118,70 +233,6 @@ returning an SDL_true into CL's boolean type system."
                            (error (error result)))))))
            (error "No main thread, did you call SDL_Init?")))))
 
-(defun handle-message (msg)
-  (let ((fun (car msg))
-        (chan (cdr msg))
-        (condition))
-    (handler-bind ((sdl-continue
-                     (lambda (c)
-                       (declare (ignore c))
-                       (when chan (sendmsg chan nil))
-                       (return-from handle-message)))
-                   (sdl-quit
-                     (lambda (c)
-                       (declare (ignore c))
-                       (quit)
-                       (return-from handle-message))))
-      (handler-bind ((error (lambda (e) (setf condition e))))
-        (if chan
-            (sendmsg chan (multiple-value-list (funcall fun)))
-            (funcall fun))))))
-
-(defun recv-and-handle-message ()
-  (let ((msg (recvmsg *main-thread-channel*)))
-    (handle-message msg)))
-
-(defun get-and-handle-messages ()
-  (loop as msg = (and *main-thread-channel*
-                      (getmsg *main-thread-channel*))
-        while msg do
-          (handle-message msg)))
-
-(defmacro without-fp-traps (&body body)
-  #+sbcl
-  `(sb-int:with-float-traps-masked (:underflow
-                                    :overflow
-                                    :inexact
-                                    :invalid
-                                    :divide-by-zero) ,@body)
-  #-sbcl
-  `(progn ,@body))
-
-(defun sdl-main-thread ()
-  (without-fp-traps
-    (let ((*main-thread* (bt:current-thread))
-          #+sdl2::sdl2-swank (swank:*sldb-quit-restart* 'continue)
-          #+sdl2::sdl2-slynk (slynk:*sly-db-quit-restart* 'continue))
-      (loop while *main-thread-channel* do
-        (block loop-block
-          (restart-bind ((continue (lambda (&optional v)
-                                     (declare (ignore v))
-                                     (signal 'sdl-continue))
-                                   :report-function
-                                   (lambda (stream)
-                                     (format stream "Return to the SDL2 main loop.")))
-                         (abort (lambda (&optional v)
-                                  (declare (ignore v))
-                                  (signal 'sdl-quit))
-                                :report-function
-                                (lambda (stream)
-                                  (format stream "Abort, quitting SDL2 entirely."))))
-            (recv-and-handle-message)))))))
-
-(defun ensure-main-channel ()
-  (unless *main-thread-channel*
-    (setf *main-thread-channel* (make-channel))))
-
 (defun make-this-thread-main (&optional function)
   "Designate the current thread as the SDL2 main thread.  This
 function will not return until `SDL2:QUIT` is handled.  Users of this
@@ -196,6 +247,7 @@ does **not** return just because `FUNCTION` returns; it still requires
 
 This does **not** call `SDL2:INIT` by itself.  Do this either with
 `FUNCTION`, or from a separate thread."
+  (setup-main-thread)
   (ensure-main-channel)
   (when (functionp function)
     (sendmsg *main-thread-channel* (cons function nil)))
@@ -203,38 +255,13 @@ This does **not** call `SDL2:INIT` by itself.  Do this either with
 
 (defun init (&rest sdl-init-flags)
   "Initialize SDL2 with the specified subsystems. Initializes everything by default."
-  (unless *wakeup-event*
-    (setf *wakeup-event* (alloc 'sdl2-ffi:sdl-event)))
-  (unless *main-thread-channel*
-    (ensure-main-channel)
 
-    ;; If we did not have a main-thread channel, make a default main
-    ;; thread.
-    #-(and ccl darwin)
-    (setf *the-main-thread* (bt:make-thread #'sdl-main-thread :name "SDL2 Main Thread"))
-
-    ;; On OSX, we need to run in the main thread; CCL allows us to
-    ;; safely do this.  On other platforms (mainly GLX?), we just need
-    ;; to run in a dedicated thread.
-    #+(and ccl darwin)
-    (let ((thread (find 0 (ccl:all-processes) :key #'ccl:process-serial-number)))
-      (setf *the-main-thread* thread)
-      (ccl:process-interrupt thread #'sdl-main-thread)))
   (in-main-thread (:no-event t)
-    ;; HACK! glutInit on OSX uses some magic undocumented API to
-    ;; correctly make the calling thread the primary thread. This
-    ;; allows cl-sdl2 to actually work. Nothing else seemed to
-    ;; work at all to be honest.
-    #+(and ccl darwin)
-    (cl-glut:init)
     (let ((init-flags (autowrap:mask-apply 'sdl-init-flags sdl-init-flags)))
       (check-rc (sdl-init init-flags))
       (unless *lisp-message-event*
         (setf *lisp-message-event* (sdl-register-events 1))
         (setf (c-ref *wakeup-event* sdl2-ffi:sdl-event :type) *lisp-message-event*)))))
-
-(defun was-init (&rest flags)
-  (/= 0 (sdl-was-init (autowrap:mask-apply 'sdl-init-flags flags))))
 
 (defun quit ()
   "Shuts down SDL2."
@@ -253,20 +280,10 @@ This does **not** call `SDL2:INIT` by itself.  Do this either with
 
 (defmacro with-init ((&rest sdl-init-flags) &body body)
   `(progn
-     (init ,@sdl-init-flags)
      (unwind-protect
-          (in-main-thread () ,@body)
-       (quit))))
-
-(defun niy (message)
-  (error "SDL2 Error: Construct Not Implemented Yet: ~A" message))
-
-(defun version ()
-  (c-let ((ver sdl2-ffi:sdl-version :free t))
-    (sdl-get-version (ver &))
-    (values (ver :major) (ver :minor) (ver :patch))))
-
-(defun version-wrapped ()
-  (values sdl2-ffi:+sdl-major-version+
-          sdl2-ffi:+sdl-minor-version+
-          sdl2-ffi:+sdl-patchlevel+))
+          (make-this-thread-main
+           (lambda ()
+             (progn
+               (init ,@sdl-init-flags)
+               ,@body
+               (quit)))))))
