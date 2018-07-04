bc982807e7feaf8386612cb42cc1823c3a6ed71f
Nisar Ahmad <nisarahmad1324@gmail.com>
2018-07-04 10:46:05 +0530

    Implement get-global-mouse-state and global-mouse-state-p.
    Also fixes the docstring of mouse-state-p.

--- src/mouse.lisp
+++ src/mouse.lisp
@@ -27,8 +27,23 @@ window and BITMASK has bit i from the right set if and only if mouse button i is
       (values x y buttons))))
 
 (defun mouse-state-p (button)
-  "Whether the mouse button numbered BUTTON is pressed. 1 indicates the left mouse button, 2 the
-middle mouse button and 3 the right mouse button."
+  "Whether the mouse button numbered BUTTON is pressed inside the focused window. 1 indicates the
+left mouse button, 2 the middle mouse button and 3 the right mouse button."
   (let ((buttons (sdl2-ffi.functions:sdl-get-mouse-state nil nil))
+        (mask (ash 1 (1- button))))
+    (plusp (logand buttons mask))))
+
+(defun get-global-mouse-state ()
+  "Returns (X Y BITMASK) where X and Y are positions of the mouse cursor relative to the desktop
+and BITMASK has bit i from the right set if and only if mouse button i is pressed."
+  (c-with ((x :int) (y :int))
+    (let ((buttons (sdl2-ffi.functions:sdl-get-global-mouse-state (x &) (y &))))
+      (values x y buttons))))
+
+(defun global-mouse-state-p (button)
+  "Whether the mouse button numbered BUTTON is pressed. 1 indicates the left mouse button,
+2 the middle mouse button and 3 the right mouse button. This function works relative to desktop
+and can be used even when there is no SDL window open."
+  (let ((buttons (sdl2-ffi.functions:sdl-get-global-mouse-state nil nil))
         (mask (ash 1 (1- button))))
     (plusp (logand buttons mask))))
